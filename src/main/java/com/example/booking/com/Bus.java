c917e9ecc2638511e35fc6df7d160546398d1b15
Akash aws <akashaws2022@gmail.com>
2023-10-02 22:42:49 +0530

    hello

---
+++ src/main/java/com/example/booking/com/Bus.java
@@ -0,0 +1,12 @@
+package com.example.booking.com;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class Bus {
+
+    @GetMapping("/Bus")
+    public String getData() {
+        return ("Click here for Bus booking");
+    }
+}
